1f9b40e935b96a6e7c5e1de0984f477b480ebd48
kombat13 <58089044+kombat13@users.noreply.github.com>
2020-02-24 00:36:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,37 @@
+button1.onclick = function () {
+    console.log("+ 1%");
+    changebar(1);
+};
+button3.onclick = function () {
+    console.log("+ 3%");
+    changebar(3);
+};
+button7.onclick = function () {
+    console.log("+ 7%");
+    changebar(7);
+};
+
+
+function changebar(addprocent) {
+    let obj = {}
+    obj = document.getElementById("myprogressbar").style.width;
+    let curprocent = 0
+    curprocent = Number(obj.slice(0, obj.length - 1)) + addprocent
+    newprocent = curprocent + "%";
+    console.log('= ' + newprocent);
+    let message = `
+          <div name="MyProgresBar" id="myprogressbar" class="progress-bar progress-bar-striped bg-success"
+            role="progressbar" style="width: ` + newprocent + `;">
+          </div>
+    `
+    $("div#result").html(message);
+}
+
+
+
+function init() {
+    console.log("скрипт подгрузился");
+}
+
+
+$(document).ready(init);
